21d8be0c9d9c36916abbc536ad3931f1537daeb3
taewan28 <twk0223@gmail.com>
2024-03-08 14:35:28 +0900

    update

--- src/main/java/org/iclass/controller/RequestControllerMapping.java
+++ src/main/java/org/iclass/controller/RequestControllerMapping.java
@@ -3,9 +3,12 @@ package org.iclass.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-
+import org.iclass.controller.community.CommentsController;
+import org.iclass.controller.community.DeleteController;
 import org.iclass.controller.community.ListController;
 import org.iclass.controller.community.ReadController;
+import org.iclass.controller.community.UpdateController;
+import org.iclass.controller.community.UpdateViewController;
 import org.iclass.controller.community.WriteController;
 import org.iclass.controller.community.WriteViewController;
 import org.iclass.controller.member.LoginActionController;
@@ -22,10 +25,10 @@ public class RequestControllerMapping {
 		mapping.put(new RequestKeyValue("/community/write","POST"), new WriteController() );
 //		//게시판 글 상세보기 ,수정하기,삭제하기
 		mapping.put(new RequestKeyValue("/community/read", "GET"), new ReadController() );
-//		mapping.put(new RequestKeyValue("/community/update","GET"), new UpdateViewController() );
-//		mapping.put(new RequestKeyValue("/community/update","POST"), new UpdateController() );
-//		mapping.put(new RequestKeyValue("/community/delete","GET"), new DeleteController() );		//오류 처리 위해 POST로 변경
-//		mapping.put(new RequestKeyValue("/community/comments","POST"), new CommentsController());
+		mapping.put(new RequestKeyValue("/community/update","GET"), new UpdateViewController() );
+		mapping.put(new RequestKeyValue("/community/update","POST"), new UpdateController() );
+		mapping.put(new RequestKeyValue("/community/delete","GET"), new DeleteController() );		//오류 처리 위해 POST로 변경
+		mapping.put(new RequestKeyValue("/community/comments","POST"), new CommentsController());
 		//회원가입
 //		mapping.put(new RequestKeyValue("/join","GET"), new MemberJoinController());
 //		mapping.put(new RequestKeyValue("/join","POST"), new MemberSaveController());
